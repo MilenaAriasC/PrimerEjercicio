a6841dd17c36ffc04f20bd0448a8b46544db1600
sweetpain <adrianstuar0229@gmail.com>
2018-08-02 14:48:26 -0500

    vista 2

---
+++ PrimerEjercicio/view2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PrimerEjercicio
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class view2 : ContentPage
+	{
+		public view2 ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
